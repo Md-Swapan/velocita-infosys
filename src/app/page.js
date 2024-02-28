00c98f846e843862ee1e49b212dd6d2e454b8d8f
Md-Swapan <swapan@excelitai.com>
2024-02-28 14:14:17 +0600

    experience update

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "@/components/sections/Navbar/Navbar";
 import Header from "@/components/sections/Header/Header";
+import Experience from "@/components/sections/Experience/Experience";
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
       <Header />
+      <Experience/>
     </main>
   );
 }
--- src/components/sections/Experience/Experience.jsx
+++ src/components/sections/Experience/Experience.jsx
@@ -1,20 +1,39 @@
 import React from "react";
+import "./Experience.css";
+import Image from "next/image";
+import icon from "../../../assets/images/icons/Mask group.png"
 
 const Experience = () => {
   return (
     <>
       <div className="experience_section">
-        <div className="experience_content_container">
+        <div className="experience_content_container container">
           <div className="year_of_experience">
             <h1>10 +</h1>
             <h5>YEAR OF EXPERIENCE</h5>
           </div>
           <div className="real_solution_content">
-            <h2>
+            <h3>
               Real Solution <br /> <span>For Your Business</span>
-            </h2>
+            </h3>
 
-            <p>As a leading IT consulting firm, we help business to reinvent and excel by </p>
+            <p>
+              As a leading IT consulting firm, we help businesses to reinvent
+              and excel by establishing complete reliance on information
+              technology and offering high quality technology consulting
+              services.
+            </p>
+
+            <div className="d-flex">
+              <div>
+                <Image src={icon} />
+                <span>Creative Concept Execution</span>
+              </div>
+              <div>
+                <Image src={icon2} />
+                <span>Amazing Business Solution</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
