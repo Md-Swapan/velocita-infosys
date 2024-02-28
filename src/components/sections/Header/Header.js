8134ab8df7192499f7a76ddc712688e0804331f2
istiak <istiak@gmail.com>
2024-02-28 12:35:08 +0600

    [done] menu and navbar

---
+++ src/components/sections/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import BannerImage from '/public/bannerimg.png'
+import Image from 'next/image';
+import './header.css'
+const Header = () => {
+    return (
+        <header style={{ minHeight: "400px" }} className='container mt-5'>
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="banner_content">
+                        <h1>We constantly 
+                            <span> innovate to
+                            create solution</span>
+                            </h1>
+                            <p>As a leading IT consulting firm, we help businesses to reinvent andexcel by establishing complete relianceon information technology andoffering high quality technology consulting services.</p>
+                            <button className='get_start_btn me-2'>Get Start</button>
+                            <button className='get_start_btn outlined_btn'>Watch Now</button>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex align-content-center justify-content-center">
+                    <div>
+                        <Image src={BannerImage} width={"100%"} />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/components/sections/Navbar/Navbar.jsx
+++ src/components/sections/Navbar/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import Link from "next/link";
+import './navbar.css'
+import Logo from '/public/logo.png'
+import Image from "next/image";
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className="velocita_nav container mt-3">
+       <div className="velocita_logo">
+       <Image width={270} src={Logo} />
+       </div>
+       <ul className="d-none d-md-flex">
+          <li><Link href={"#"}>Home</Link></li>
+          <li><Link href={"#"}>Services</Link></li>
+          <li><Link href={"#"}>About Us</Link></li>
+          <li><Link href={"#"}>Portfolio</Link></li>
+          <li><Link href={"#"}>Blog</Link></li>
+          <li><Link href={"#"}>Career</Link></li>
+          <li><Link href={"#"}>Team</Link></li>
+          <li><Link className="link_btn" href={"#"}>Contact Us</Link></li>
+       </ul>
+    </nav>
   );
 };
 
