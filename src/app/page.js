230f2660eeba36ddff7051b42da29d79df622328
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2024-03-03 15:10:50 +0600

    fotter part done

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Header from "@/components/sections/Header/Header";
 import Services from "@/components/sections/Services/Services";
 import ProjectIdea from "@/components/sections/ProjectIdea/ProjectIdea";
 import ExploreUs from "@/components/sections/ExploreUs/ExploreUs";
+import Footer from "@/components/sections/Footer/Footer";
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -14,6 +15,7 @@ export default function Home() {
       <ExploreUs />
       <Services />
       <ProjectIdea />
+      <Footer />
     </main>
   );
 }
