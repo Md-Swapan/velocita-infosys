f86da539733cf3e36eadd0fe2d9c5cc77dc83bac
Md-Swapan <swapan@excelitai.com>
2024-03-03 18:39:53 +0600

    login and admin panel add

---
+++ src/app/(routes)/adminPanel/create-job-post/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      create job post
+    </div>
+  );
+};
+
+export default page;
--- src/app/(routes)/login/page.jsx
+++ src/app/(routes)/login/page.jsx
@@ -1,36 +1,77 @@
-"use client"
+"use client";
 import React, { useState } from "react";
+import logo from "../../../assets/images/Velocita-logo1 1.png";
+import Image from "next/image";
+import "./login.css";
+import Link from "next/link";
+import { baseURL } from "@/assets/baseURL/baseURL";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const page = () => {
-  const [error, setError] = useState("");
-  const [loginFailedMassage, setLoginFailedMassage] = useState("");
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
   };
 
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const url = baseURL + "api/v1/auth/admin-login";
+
+      await axios.post(url, data).then((res) => {
+        if (res.data.success == true) {
+          localStorage.setItem("token", res.data.data.token);
+          router.push("/");
+        }
+      });
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
 
   return (
-    <div className="login_section_container">
-      <div className="login_form">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            name="admin_email"
-            onChange={handleChange}
-            required
-          />
-          <input
-            type={passwordType}
-            placeholder="Password"
-            name="password"
-            onChange={handleChange}
-            required
-          />
+    <div className="login_section">
+      <div className="login_container">
+        <Image width={300} src={logo} alt="logo" />
+        <div className="login_form">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              required
+            />
 
-          <button type="submit">Login</button>
-        </form>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+
+        <Link href="/">
+          <button>Back To Home</button>
+        </Link>
       </div>
     </div>
   );
