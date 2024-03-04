cd4e5f18f20cecb7daf796789fc959bb52f7d69f
Md-Swapan <swapan@excelitai.com>
2024-03-04 15:37:18 +0600

    create job post section done

--- src/app/(routes)/adminPanel/create-job-post/page.jsx
+++ src/app/(routes)/adminPanel/create-job-post/page.jsx
@@ -1,9 +1,220 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
+import "./createJobPost.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { baseURL } from "@/assets/baseURL/baseURL";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const page = () => {
+  const [checked, setChecked] = React.useState(false);
+  const [datePosted, setDatePosted] = useState(new Date());
+  const [applicationDeadline, setApplicationDeadline] = useState(new Date());
+  const [jobData, setJobData] = useState({
+    job_code: "",
+    job_title: "",
+    department: "",
+    experience: "",
+    job_summary: "",
+    requirements: "",
+    additional_requirements: "",
+    responsibilities: "",
+    compensation_benefits: "",
+    date_posted: `${datePosted}`,
+    application_deadline: `${applicationDeadline}`,
+    email: "",
+    phone_number: "",
+    location: "",
+    visible: `${checked}`,
+  });
+  
+
+  console.log(datePosted);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setJobData({ ...jobData, [input.name]: input.value });
+  };
+
+  const handleCheckChange = (e) => {
+    setJobData({...jobData, visible: `${e.target.checked}`});
+  };
+
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    try {
+      const url = baseURL + "api/v1/career";
+      await axios.post(url, jobData).then((res) => {
+        console.log(res);
+      });
+
+    } catch (error) {
+
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+
+    }
+  };
+
+
   return (
-    <div>
-      create job post
+    <div className="create_job_post_section">
+      <div className="create_job_post_container">
+        <div className="create_job_form">
+          <form onSubmit={handleSubmit}>
+            <div className="job_overview_form_container">
+              <h4>Job Overview</h4>
+              <div className="job_overview_input_container">
+                <input
+                  type="text"
+                  placeholder="Job Code"
+                  name="job_code"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Job Title"
+                  name="job_title"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Department"
+                  name="department"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Experience"
+                  name="experience"
+                  onChange={handleChange}
+                />
+
+                <div className="mt-3">
+                  <label htmlFor="">Job Summary</label>
+                  <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Job Summary</p>"
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      setJobData({ ...jobData, job_summary: data });
+                    }}
+                  />
+                </div>
+                <div className="mt-3">
+                  <label htmlFor="">Requirements</label>
+                  <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Requirements</p>"
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      setJobData({ ...jobData, requirements: data });
+                    }}
+                  />
+                </div>
+                <div className="mt-3">
+                  <label htmlFor="">Additional Requirements</label>
+                  <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Additional Requirements</p>"
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      setJobData({ ...jobData, additional_requirements: data });
+                    }}
+                  />
+                </div>
+                <div className="mt-3">
+                  <label htmlFor="">Responsibilities</label>
+                  <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Responsibilities</p>"
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      setJobData({ ...jobData, responsibilities: data });
+                    }}
+                  />
+                </div>
+                <div className="mt-3">
+                  <label htmlFor="">Compensation Benefits</label>
+                  <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Compensation Benefits</p>"
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      setJobData({ ...jobData, compensation_benefits: data });
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="general_information_form_container">
+              <h4>General Information</h4>
+              <div className="general_information_input_container">
+                <input
+                  type="text"
+                  placeholder="Date Of Posted"
+                  name="date_posted"
+                  onChange={handleChange}
+                />
+                 {/* <div>
+                  <label htmlFor="">Date Of Posted</label>
+                 <DatePicker selected={datePosted} onChange={(date) => setDatePosted(date)} />
+                 </div>
+                 <div>
+                  <label htmlFor="">Application Deadline</label>
+                 <DatePicker selected={applicationDeadline} onChange={(date) => setApplicationDeadline(date)} />
+                 </div> */}
+                 
+                <input
+                  type="text"
+                  placeholder="Application Deadline"
+                  name="application_deadline"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Phone Number"
+                  name="phone_number"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Location"
+                  name="location"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="visible_checkbox">
+                <input
+                  value="test"
+                  type="checkbox"
+                  onChange={handleCheckChange}
+                />
+                <span htmlFor="">Visible</span>
+              </div>
+            <button type="submit" className="create_job_submit_btn">
+              Submit
+            </button>
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/sections/Navbar/Navbar.jsx
+++ src/components/sections/Navbar/Navbar.jsx
@@ -1,53 +1,118 @@
-"use client"
+"use client";
 import Link from "next/link";
-import './navbar.css'
-import Logo from '/public/logo.png'
+import "./navbar.css";
+import Logo from "/public/logo.png";
 import Image from "next/image";
 import MenuIcon from "/public/menu.png";
 import CloseIcon from "/public/close.png";
 import { useState } from "react";
 
-
-
 const Navbar = () => {
-  const [openMenu,setOpenMenu]=useState(false)
-
+  const [openMenu, setOpenMenu] = useState(false);
+  
   const token = localStorage.getItem("token");
+  const LogoutHandler = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
 
   return (
     <nav className="velocita_nav container mt-3">
-       <div className="velocita_logo">
-       <Image  src={Logo} />
-       </div>
-       {/* for mobile device  */}
-       <Image onClick={()=>setOpenMenu(true)} className="menu_icon d-md-none" width={30} src={MenuIcon} />
-       <div className={`mobile_menu d-md-none ${openMenu?'show':'hide'}`}>
-         <Image onClick={()=>setOpenMenu(false)} className="menu_icon d-md-none mb-5" width={30} src={CloseIcon} />
-          <p><Link href={"#"}>Home</Link></p>
-          <p><Link href={"#"}>Services</Link></p>
-          <p><Link href={"#"}>About Us</Link></p>
-          <p><Link href={"#"}>Portfolio</Link></p>
-          <p><Link href={"#"}>Career</Link></p>
-          <p><Link href={"#"}>Team</Link></p>
-          <p><Link  className="mobile_link_btn" href={"#"}>Contact Us</Link></p>
-         {token && <p><Link  href={"/adminPanel"}>Admin Panel</Link></p>}
-          <p><Link   href={"/login"}>Login</Link></p>
-       </div>
-        {/* for mobile device  */}
+      <div className="velocita_logo">
+        <Image src={Logo} />
+      </div>
+      {/* for mobile device  */}
+      <Image
+        onClick={() => setOpenMenu(true)}
+        className="menu_icon d-md-none"
+        width={30}
+        src={MenuIcon}
+      />
+      <div className={`mobile_menu d-md-none ${openMenu ? "show" : "hide"}`}>
+        <Image
+          onClick={() => setOpenMenu(false)}
+          className="menu_icon d-md-none mb-5"
+          width={30}
+          src={CloseIcon}
+        />
+        <p>
+          <Link href={"#"}>Home</Link>
+        </p>
+        <p>
+          <Link href={"#"}>Services</Link>
+        </p>
+        <p>
+          <Link href={"#"}>About Us</Link>
+        </p>
+        <p>
+          <Link href={"#"}>Portfolio</Link>
+        </p>
+        <p>
+          <Link href={"#"}>Career</Link>
+        </p>
+        <p>
+          <Link href={"#"}>Team</Link>
+        </p>
+        <p>
+          <Link className="mobile_link_btn" href={"#"}>
+            Contact Us
+          </Link>
+        </p>
+        {token && (
+          <p>
+            <Link href={"/adminPanel"}>Admin Panel</Link>
+          </p>
+        )}
+        {token ? (
+          <p onClick={LogoutHandler}>Logout</p>
+        ) : (
+          <p>
+            <Link href="/login">Login</Link>
+          </p>
+        )}
+      </div>
+      {/* for mobile device  */}
 
-        {/* for desktop device */}
-       <ul className="d-none d-md-flex">
-          <li><Link href={"#"}>Home</Link></li>
-          <li><Link href={"#"}>Services</Link></li>
-          <li><Link href={"#"}>About Us</Link></li>
-          <li><Link href={"#"}>Portfolio</Link></li>
-          <li><Link href={"#"}>Career</Link></li>
-          <li><Link href={"#"}>Team</Link></li>
-          <li><Link className="link_btn" href={"#"}>Contact Us</Link></li>
-          {token && <li><Link  href={"/adminPanel"}>Admin Panel</Link></li>}
-          <li><Link   href="/login">Login</Link></li>
-       </ul>
-       {/* for desktop device */}
+      {/* for desktop device */}
+      <ul className="d-none d-md-flex">
+        <li>
+          <Link href={"#"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"#"}>Services</Link>
+        </li>
+        <li>
+          <Link href={"#"}>About Us</Link>
+        </li>
+        <li>
+          <Link href={"#"}>Portfolio</Link>
+        </li>
+        <li>
+          <Link href={"#"}>Career</Link>
+        </li>
+        <li>
+          <Link href={"#"}>Team</Link>
+        </li>
+        <li>
+          <Link className="link_btn" href={"#"}>
+            Contact Us
+          </Link>
+        </li>
+        {token && (
+          <li>
+            <Link href={"/adminPanel"}>Admin Panel</Link>
+          </li>
+        )}
+        {token ? (
+          <li onClick={LogoutHandler}>Logout</li>
+        ) : (
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+        )}
+      </ul>
+      {/* for desktop device */}
     </nav>
   );
 };
