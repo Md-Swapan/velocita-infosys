d370e283a7d1db0c71ba35a706a1eeedc0945ed5
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-03-06 16:12:00 +0600

    [DONE] loading added.

--- src/app/(routes)/create-job-post/page.jsx
+++ src/app/(routes)/create-job-post/page.jsx
@@ -1,9 +1,9 @@
 "use client"
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import dynamic from "next/dynamic"
 
-const CreateJobPostComponet = dynamic(
+const CreateJobPostComponent = dynamic(
   () => {
     return import(
       "@/app/(routes)/create-job-post/_components/CreateJobPostComponet"
@@ -12,10 +12,26 @@ const CreateJobPostComponet = dynamic(
   { ssr: false }
 )
 
-export default function page() {
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  // This useEffect will set isLoading to false once the component is loaded
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+
   return (
-    <div>
-      <CreateJobPostComponet />
+    <div
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      {/* Conditionally render the loading indicator */}
+      {isLoading ? (
+        <h2 className="text-center text-white pt-5">Loading...</h2>
+      ) : (
+        <CreateJobPostComponent />
+      )}
     </div>
   )
 }
